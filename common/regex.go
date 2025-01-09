e5da3e93fcdc9e43aa76f41a01f6e0e1e5fc7815
EbrahimKhodadadi <ebrahimkhodadadideveloper@gmail.com>
2025-01-09 14:10:06 +0330

    make isIraninaPhoneNumber function public

--- common/regex.go
+++ common/regex.go
@@ -2,7 +2,7 @@ package common
 
 import "regexp"
 
-func isIranianNumber(phoneNumber string) bool {
+func IsIranianNumber(phoneNumber string) bool {
 	// Regex pattern for Iranian phone numbers: starts with +98 or 09, followed by 9 digits
 	// Example: +989123456789 or 09123456789
 	iranianNumberPattern := `^(?:\+98|0)9\d{9}$`
