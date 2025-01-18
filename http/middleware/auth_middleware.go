4887c1c2c118d02354bc6a502c3bcef54397843a
EbrahimKhodadadi <ebrahimkhodadadideveloper@gmail.com>
2025-01-18 17:42:09 +0330

    implement auth middleware

--- http/middleware/auth_middleware.go
+++ http/middleware/auth_middleware.go
@@ -1,53 +1,61 @@
 package middleware
 
-// import (
-// 	"fmt"
-// 	"net/http"
-// 	"strings"
+import (
+	"strings"
 
-// 	"github.com/gofiber/fiber/v2"
-// 	"github.com/golang-jwt/jwt"
-// 	"github.com/minisource/common_go/constants"
-// 	"github.com/minisource/common_go/http/services"
-// )
+	"github.com/gofiber/fiber/v2"
+	"github.com/minisource/apiclients/auth/auth"
+	"github.com/minisource/apiclients/auth/auth/models"
+	"github.com/minisource/common_go/constants"
+	helper "github.com/minisource/common_go/http/helper"
+	"github.com/minisource/common_go/service_errors"
+)
 
-// func Authentication(cfg *services.JWTConfig) fiber.Handler {
-// 	// var tokenUsecase = usecase.NewTokenUsecase(cfg)
+func Authentication(authApiClient *helper.APIClient) fiber.Handler {
+	auth.NewAuthService(authApiClient)
+	service := auth.GetAuthService()
 
-// 	return func(c *fiber.Ctx) error {
-// 		// var err error
-// 		// claimMap := map[string]interface{}{}
-// 		// auth := c.GetHeader(constants.AuthorizationHeaderKey)
-// 		// token := strings.Split(auth, " ")
-// 		// if auth == "" || len(token) < 2 {
-// 		// 	err = &service_errors.ServiceError{EndUserMessage: service_errors.TokenRequired}
-// 		// } else {
-// 		// 	claimMap, err = tokenUsecase.GetClaims(token[1])
-// 		// 	if err != nil {
-// 		// 		switch err.(*jwt.ValidationError).Errors {
-// 		// 		case jwt.ValidationErrorExpired:
-// 		// 			err = &service_errors.ServiceError{EndUserMessage: service_errors.TokenExpired}
-// 		// 		default:
-// 		// 			err = &service_errors.ServiceError{EndUserMessage: service_errors.TokenInvalid}
-// 		// 		}
-// 		// 	}
-// 		// }
-// 		// if err != nil {
-// 		// 	c.AbortWithStatusJSON(http.StatusUnauthorized, helper.GenerateBaseResponseWithError(
-// 		// 		nil, false, helper.AuthError, err,
-// 		// 	))
-// 		// 	return
-// 		// }
+	return func(c *fiber.Ctx) error {
+		var err error
+		var claimMap *models.ValidateAuthTokenRes
+		auth := c.Get(constants.AuthorizationHeaderKey)
+		token := strings.Split(auth, " ")
+		if auth == "" || len(token) < 2 {
+			err = &service_errors.ServiceError{EndUserMessage: service_errors.TokenRequired}
+		} else {
+			claimMap, err = service.ValidateAccessToken(models.ValidateAccessTokenRequest{AccessToken: token[1]})
+			if err != nil {
+				return err
+			}
+		}
+		if err != nil {
+			return c.Status(fiber.StatusUnauthorized).JSON(
+				helper.GenerateBaseResponseWithError(nil, false, helper.AuthError, err),
+			)
+		}
 
-// 		// c.Set(constant.UserIdKey, claimMap[constant.UserIdKey])
-// 		// c.Set(constant.FirstNameKey, claimMap[constant.FirstNameKey])
-// 		// c.Set(constant.LastNameKey, claimMap[constant.LastNameKey])
-// 		// c.Set(constant.UsernameKey, claimMap[constant.UsernameKey])
-// 		// c.Set(constant.EmailKey, claimMap[constant.EmailKey])
-// 		// c.Set(constant.MobileNumberKey, claimMap[constant.MobileNumberKey])
-// 		// c.Set(constant.RolesKey, claimMap[constant.RolesKey])
-// 		// c.Set(constant.ExpireTimeKey, claimMap[constant.ExpireTimeKey])
+		if userId, ok := claimMap.Claims[constants.UserIdKey].(string); ok {
+			c.Set(constants.UserIdKey, userId)
+		}
+		if firstName, ok := claimMap.Claims[constants.FirstNameKey].(string); ok {
+			c.Set(constants.FirstNameKey, firstName)
+		}
+		if lastName, ok := claimMap.Claims[constants.LastNameKey].(string); ok {
+			c.Set(constants.LastNameKey, lastName)
+		}
+		if username, ok := claimMap.Claims[constants.UsernameKey].(string); ok {
+			c.Set(constants.UsernameKey, username)
+		}
+		if email, ok := claimMap.Claims[constants.EmailKey].(string); ok {
+			c.Set(constants.EmailKey, email)
+		}
+		if phoneNumber, ok := claimMap.Claims[constants.PhoneNumberKey].(string); ok {
+			c.Set(constants.PhoneNumberKey, phoneNumber)
+		}
+		if expireTime, ok := claimMap.Claims[constants.ExpireTimeKey].(string); ok {
+			c.Set(constants.ExpireTimeKey, expireTime)
+		}
 
-// 		return c.Next()
-// 	}
-// }
+		return c.Next()
+	}
+}
