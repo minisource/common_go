22e76f3dca14f9c00de12bfc0c73ea39ee05705b
ebrahim <ebrahimbrooz@gmail.com>
2025-07-25 03:08:15 +0330

    add ClientIdKey constant for OAuth token generation

--- constants/constanst.go
+++ constants/constanst.go
@@ -4,6 +4,7 @@ const (
 	// Claims
 	AuthorizationHeaderKey string = "Authorization"
 	UsernameKey            string = "Username"
+	ClientIdKey            string = "ClientId"
 	ExpireTimeKey          string = "Exp"
 
 	GenerateTokenRoute = "/api/v1/oauth/GenerateToken"
